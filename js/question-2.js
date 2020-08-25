3df6776ea576cb0dcc35dc57e2fe28fed5f3a7f1
Strand-Made <60408983+Strand-Made@users.noreply.github.com>
2020-08-25 17:41:47 +0200

    Update question-2.js

--- js/question-2.js
+++ js/question-2.js
@@ -2,17 +2,29 @@
 const urlApi =
   "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating";
 
+const gamesContainer = document.querySelector(".gamesContainer");
+
 async function getGames() {
   const response = await fetch(urlApi);
 
   const result = await response.json();
-  const games = result.all;
 
-  for (let i = 0; i < urlApi.length; i++) {
+  console.log(result);
+
+  const videoGames = result.all;
+
+  console.log(videoGames);
+
+  //gamesContainer.innerHTML = "";
+
+  for (let i = 0; i < videoGames.length; i++) {
+    console.log(videoGames[i].text);
+
     if (i === 9) {
       break;
     }
-    console.log([i]);
+
+    gamesContainer.innerHTML += `<div class="result"> ${videoGames[i].text} </div>`;
   }
 }
 
