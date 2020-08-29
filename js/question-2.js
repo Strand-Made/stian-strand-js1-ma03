d6830054e049af42b8e6dd826c039a13fd1ab428
Strand-Made <60408983+Strand-Made@users.noreply.github.com>
2020-08-29 11:48:32 +0200

    added loader.css

--- js/question-2.js
+++ js/question-2.js
@@ -21,7 +21,7 @@ async function getRawg() {
             break;
         }
 
-        rawgContainer.innerHTML += `<div class="result"><h3>${games[i].name}</h3> <p> Rating: ${games[i].rating}</p> <p>Tags ${games.length}</p></div>`;
+        rawgContainer.innerHTML += `<div class="result"><h3>${games[i].name}</h3> <p> Rating: ${games[i].rating}</p> <p>Tags ${games[i].tags.length}</p></div>`;
     }
     
 
