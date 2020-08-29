fa0b4100d2169c17b99a152d9d0360f8f3f30324
Strand-Made <60408983+Strand-Made@users.noreply.github.com>
2020-08-29 15:18:16 +0200

    Loader finished

--- js/question-2.js
+++ js/question-2.js
@@ -3,35 +3,35 @@
 const url = "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating";
 const rawgContainer = document.querySelector(".gamesContainer");
 
+
 async function getRawg() {
     // fetch api
+    try {
+        const response = await fetch(url);
 
-    const response = await fetch(url);
-
-    const apiFeed = await response.json();
+        const apiFeed = await response.json();
     
-    const games = apiFeed.results;
+        const games = apiFeed.results;
 
-    rawgContainer.innerHTML= "";
+        rawgContainer.innerHTML= "";
 
-    for(let i = 0; i < games.length; i++) {
-        console.log(games[i]);
+        for(let i = 0; i < games.length; i++) {
+            console.log(games[i]);
 
-        if ( i === 8) {
+            if ( i === 8) {
             break;
-        }
+            }
 
-        rawgContainer.innerHTML += `<div class="result"><h3>${games[i].name}</h3> <p> Rating: ${games[i].rating}</p> <p>Tags ${games[i].tags.length}</p></div>`;
-    }
+            rawgContainer.innerHTML += `<div class="result"><h3>${games[i].name}</h3> <p> Rating: ${games[i].rating}</p> <p>Tags: ${games[i].tags.length}</p></div>`;
+        }   
     
-
-
-
-    
-
+    } catch (error) {
+        rawgContainer.innerHTML = errorMessage("An error ran up on me while calling the API");
+        }   
 }
 
 getRawg();
+
 
 
 
